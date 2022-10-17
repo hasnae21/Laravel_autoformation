ed7e78ddc0e29503e28096fb60c227fdce926deb
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-17 11:31:19 +0100

    save

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <title>Document</title>
+    <title>test</title>
 </head>
 <body>
     <h2>Hello world again</h2>
