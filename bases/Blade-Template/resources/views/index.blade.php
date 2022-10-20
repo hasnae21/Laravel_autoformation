cc5da0e585a6b26426966e0a9bed8ca4924277cd
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-20 09:45:05 +0100

    save
    save

--- bases/Blade-Template/resources/views/index.blade.php
+++ bases/Blade-Template/resources/views/index.blade.php
@@ -1,30 +1,38 @@
+<!-- how to include a view file  -->
 @include('titre')
 
-{{-- if and else --}}
 
- id = {{$name=1}}
+
+
+
+<!-- if and else  -->
+
+ <!-- id = {{$name=1}}  -->
+ age = {{$age=22}} 
+
  @if ($name == 1)
-    <h2>hello nada</h2>
+    <h2>hello There</h2>
 @else
    <h2> NULL</h2> 
 @endif 
 
-{{-- isset and empty --}}
+<!-- {{-- isset and empty --}} -->
 
 @isset($age)
-<h2>Age is {{$age}}</h2>
+<h2>I'm two {{$age}} years old</h2>
 @endisset
 
 @empty($age)
     <h2>age is empty</h2>
 @endempty
 
-{{-- loop Foreach --}}
+<!-- {{-- loop Foreach --}} -->
+
 @php
-$user = array("nada","bautaina","iman")    
+$user = array("ford","oudi","fiat")    
 @endphp
 
 @foreach ($user  as $value)
-    <p>User name id : {{ $value }}</p>
+    <p>my car type is: {{ $value }}</p>
 @endforeach
  
