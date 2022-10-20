a4316263a34ab552db89034f30bf4ef4732decc0
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-20 10:51:30 +0100

    save

--- database/database/migrations/2022_10_20_094632_create_posts_table.php
+++ database/database/migrations/2022_10_20_094632_create_posts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table -> string('title');
+            $table -> string('slug');
+            $table -> text('body');
+            $table -> string('image');
             $table->timestamps();
         });
     }
