fda78d56eb05bf1dbc9e1c6fe425d2a234065a3d
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-11-07 10:31:16 +0100

    save

--- Relationships/app/Models/Test.php
+++ Relationships/app/Models/Test.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Test extends Model
+class User extends Model
 {
     use HasFactory;
 
     /**
-     * Get the phone associated with the user.
+     * Get the phone associated with the user using the methode (hasOne())
      */
+
     public function phone()
     {
         return $this->hasOne(Phone::class);
