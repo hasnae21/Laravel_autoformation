37e1d281da0e5750d285fc9d7dc71b18350efec2
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-11-07 13:21:46 +0100

    save

--- Relationships/routes/web.php
+++ Relationships/routes/web.php
@@ -17,10 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    
+
     // $user = User::find(1);   
     // echo $user->carte->cin;  
     
+    $users = \App\User::all();
 
+    return view('users.index',compact('users'));
 
 });
