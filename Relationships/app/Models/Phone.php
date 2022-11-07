d8372bb24bf84fb9085ffa8f28ff7a3fd78ab507
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-11-07 10:41:35 +0100

    belongsTo

---
+++ Relationships/app/Models/Phone.php
@@ -0,0 +1,18 @@
+<?php
+ 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class Phone extends Model
+{
+    /**
+     * Defining The Inverse Of The Relationship
+     * Get the user that owns the phone using the methode (belongsTo())
+     */
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
